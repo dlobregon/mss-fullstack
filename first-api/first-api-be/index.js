5ef721bcfcf505234cc70988ec40222a92b75618
dlobregon <troyanrex@gmail.com>
2023-12-06 19:27:42 -0600

    add cors to api

--- first-api/first-api-be/index.js
+++ first-api/first-api-be/index.js
@@ -7,6 +7,19 @@ app.use(express.json())
 
 const apis = []
 
+
+//middleware for allos CORS
+const allowCrossDomain = (req, res, next) => {
+    res.header(`Access-Control-Allow-Origin`, `*`);
+    res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
+    res.header(`Access-Control-Allow-Headers`, `Content-Type`);
+    next();
+};
+
+
+app.use(allowCrossDomain)
+
+
 // handling the save process
 app.post('/', (req, res) => {
     const { builder, urlString } = req.body
